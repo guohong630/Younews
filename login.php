90e84bad6ede9ac675ffb2eeb473ce7d48d9d99a
si-yao <siyao.li.szeyiu@gmail.com>
2014-10-29 11:10:57 -0400

    login mod

--- login.php
+++ login.php
@@ -2,8 +2,13 @@
 <?php
 	session_start();
 ?>
+<h3> Stock Media Platform </h3>
 <form name="input" action="login_handle.php" method="post">
-Username: <input type="text" name="user">
-Password: <input type="text" name="password">
-<input type="submit" value="Submit">
+Username: <input type="text" name="user"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit" value="Login">
 </form>
+<br>
+Want an account?
+<br>
+<button type="button" onclick= "self.location='signup.php'" >Register</button>
