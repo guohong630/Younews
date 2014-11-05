e1ee436837bae0c09001eb7318ba1e3e50e00ba5
si-yao <siyao.li.szeyiu@gmail.com>
2014-11-05 17:27:03 -0500

    add unselect

--- welcome.php
+++ welcome.php
@@ -6,11 +6,22 @@
 	require_once('verify.php');//require connection.php before verifying! and should start session first!
 	$user=$_SESSION['username'];
 	$passwd=$_SESSION['password'];
+
 	//echo  "<p class = 'text'>Welcome! $user! (<a href=\"logout.php\">Logout</a>)</p><br>";
 	$sql_user = oci_parse($conn, "select user_id from users where user_name='$user'");
 	oci_execute($sql_user,OCI_DEFAULT);
 	oci_fetch_all($sql_user,$res);
 	$userid = $res['USER_ID'][0];
+
+	if(isset($_GET['rm'])){
+		if($_GET['rm']==1){
+			$stkid=$_GET['stockid'];
+			$del_sql="delete from select_stock where user_id=$userid and stock_id=$stkid";
+			$stm=oci_parse($conn, $del_sql);
+			oci_execute($stm);
+			oci_commit($conn);
+		}
+	}
 	//echo "<a href=\"news_list.php?userid=$userid\">Check your personal news!<br>";
 	$sql = oci_parse($conn, "select si.stock_id, si.stock_name, c.com_name from users u, select_stock ss, stocks_issue si,companies c where u.user_name='$user' and u.user_id=ss.user_id and ss.stock_id=si.stock_id and si.company_id=c.company_id");
 	oci_execute($sql,OCI_DEFAULT);
@@ -82,7 +93,7 @@
 		$stockid=$row[0];
 		$com=$row[2];
 		$disp = $row[2] ." (". $row[1].")";
-		echo "<a href=\"stock_data.php?stockid=$stockid&stockname=$com\">$disp</a><br>";
+		echo "<a href=\"stock_data.php?stockid=$stockid&stockname=$com\">$disp</a>    (<a href=\"welcome.php?rm=1&stockid=$stockid\">x</a>)<br>";
 	}
 	?>
 </p>
